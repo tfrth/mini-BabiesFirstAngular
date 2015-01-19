4d7204366f4f1aaf1a8f15c31a89975feecde804
Ty Frith <tyfrith@gmail.com>
2015-01-19 13:59:04 -0700

    baby angular updated

---
+++ js/mainCtrl.js
@@ -0,0 +1,10 @@
+var app = angular.module('friendsList');
+
+app.controller('mainCtrl', function($scope){
+		$scope.friends = ['nick', 'zaheed', 'katelyn', 'jeppy'];
+
+		$scope.addFriends = function() {
+			
+		}
+
+});
